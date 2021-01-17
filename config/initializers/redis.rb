76d9ff67d1b6cef2ca1c8acbdb9a347f471cb554
Sunny Ng <sng@squarespace.com>
2021-01-17 18:14:35 -0500

    Support SSL Redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,18 @@
 require 'redis'
+require 'uri'
 
-REDIS_CLIENT = Redis.new(url: ENV['REDIS_URL'] || ENV['REDISCLOUD_URL'])
+if Rails.env == 'production'
+  if ENV['REDIS_URL']
+    url = URI.parse(ENV["REDIS_URL"])
+    url.scheme = "rediss"
+    url.port = Integer(url.port) + 1
+    REDIS_CLIENT = Redis.new(url: url, driver: :ruby, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+  else
+    REDIS_CLIENT = Redis.new(url: ENV['REDISCLOUD_URL'])
+  end
+else
+  REDIS_CLIENT = Redis.new(url: ENV['REDIS_URL'])
+end
+
+
+
