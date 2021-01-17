572621ea2201fc6f37b996098e926650eac1b7d2
Sunny Ng <sng@squarespace.com>
2021-01-17 18:23:35 -0500

    Try again with the Redis SSL thing

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,10 +3,7 @@ require 'uri'
 
 if Rails.env == 'production'
   if ENV['REDIS_URL']
-    url = URI.parse(ENV["REDIS_URL"])
-    url.scheme = "rediss"
-    url.port = Integer(url.port) + 1
-    REDIS_CLIENT = Redis.new(url: url, driver: :ruby, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+    REDIS_CLIENT =Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
   else
     REDIS_CLIENT = Redis.new(url: ENV['REDISCLOUD_URL'])
   end
