8f260d6e0ed429912a0446ba0b5f281da9968ddd
Sunny Ng <sng@squarespace.com>
2021-01-19 00:24:07 -0500

    Fix autoscaler again

--- app/models/heroku_autoscaler.rb
+++ app/models/heroku_autoscaler.rb
@@ -1,8 +1,8 @@
 class HerokuAutoscaler
   MINIMUM_NUMBER_OF_DYNOS = 1
   MAXIMUM_NUMBER_OF_DYNOS = 10
-  SCALE_DOWN_THRESHOLD = ENV['AUTOSCALER_SCALE_DOWN_THRESHOLD'].to_i || 0
-  SCALE_UP_THRESHOLD = ENV['AUTOSCALER_SCALE_UP_THRESHOLD'].to_i || 49
+  SCALE_DOWN_THRESHOLD = ENV['AUTOSCALER_SCALE_DOWN_THRESHOLD']&.to_i || 0
+  SCALE_UP_THRESHOLD = ENV['AUTOSCALER_SCALE_UP_THRESHOLD']&.to_i || 49
 
   class << self
     def run
@@ -16,7 +16,7 @@ class HerokuAutoscaler
       puts "HerokuAutoscaler: #{number_of_dynos} dynos, #{jobs_in_queue} jobs"
 
       if jobs_in_queue <= SCALE_DOWN_THRESHOLD
-        last_unempty_timestamp = RedisStore.update_last_unempty_workqueue_timestamp
+        last_unempty_timestamp = RedisStore.last_unempty_workqueue_timestamp
         if last_unempty_timestamp && (Time.at(last_unempty_timestamp) - Time.current) >= 10.minutes && number_of_dynos > MINIMUM_NUMBER_OF_DYNOS
           new_quantity = number_of_dynos - 1
           heroku.formation.update(ENV['HEROKU_APP_NAME'], 'worker', {"quantity" => new_quantity})
