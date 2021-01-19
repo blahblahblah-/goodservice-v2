a17408876db558c2f8d8abb5160f2cff9dde1bfb
Sunny Ng <sng@squarespace.com>
2021-01-18 20:10:02 -0500

    Update constants

--- app/models/heroku_autoscaler.rb
+++ app/models/heroku_autoscaler.rb
@@ -1,6 +1,8 @@
 class HerokuAutoscaler
   MINIMUM_NUMBER_OF_DYNOS = 1
   MAXIMUM_NUMBER_OF_DYNOS = 10
+  SCALE_DOWN_THRESHOLD = 0
+  SCALE_UP_THRESHOLD = 49
 
   class << self
     def run
@@ -13,7 +15,7 @@ class HerokuAutoscaler
       jobs_in_queue = Delayed::Job.all.size
       puts "HerokuAutoscaler: #{number_of_dynos} dynos, #{jobs_in_queue} jobs"
 
-      if jobs_in_queue == 0
+      if jobs_in_queue == SCALE_DOWN_THRESHOLD
         last_unempty_timestamp = RedisStore.update_last_unempty_workqueue_timestamp
         if last_unempty_timestamp && (Time.at(last_unempty_timestamp) - Time.current) >= 10.minutes && number_of_dynos > MINIMUM_NUMBER_OF_DYNOS
           new_quantity = number_of_dynos - 1
@@ -21,7 +23,7 @@ class HerokuAutoscaler
           puts "HerokuAutoscaler: Scaled down to #{new_quantity} dynos"
         end
       else
-        if jobs_in_queue > 49
+        if jobs_in_queue > SCALE_UP_THRESHOLD
           if number_of_dynos < MAXIMUM_NUMBER_OF_DYNOS
             new_quantity = number_of_dynos + 1
             heroku.formation.update(ENV['HEROKU_APP_NAME'], 'worker', {"quantity" => new_quantity})
