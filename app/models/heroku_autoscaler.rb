d104b1a53904bbbfa93259260bd453affad8da37
Sunny Ng <sng@squarespace.com>
2021-01-18 21:34:19 -0500

    Make autoscaler controllable with env vars

--- app/models/heroku_autoscaler.rb
+++ app/models/heroku_autoscaler.rb
@@ -1,8 +1,8 @@
 class HerokuAutoscaler
   MINIMUM_NUMBER_OF_DYNOS = 1
   MAXIMUM_NUMBER_OF_DYNOS = 10
-  SCALE_DOWN_THRESHOLD = 0
-  SCALE_UP_THRESHOLD = 49
+  SCALE_DOWN_THRESHOLD = ENV['AUTOSCALER_SCALE_DOWN_THRESHOLD'] || 0
+  SCALE_UP_THRESHOLD = ENV['AUTOSCALER_SCALE_UP_THRESHOLD'] || 49
 
   class << self
     def run
@@ -15,7 +15,7 @@ class HerokuAutoscaler
       jobs_in_queue = Delayed::Job.all.size
       puts "HerokuAutoscaler: #{number_of_dynos} dynos, #{jobs_in_queue} jobs"
 
-      if jobs_in_queue == SCALE_DOWN_THRESHOLD
+      if jobs_in_queue <= SCALE_DOWN_THRESHOLD
         last_unempty_timestamp = RedisStore.update_last_unempty_workqueue_timestamp
         if last_unempty_timestamp && (Time.at(last_unempty_timestamp) - Time.current) >= 10.minutes && number_of_dynos > MINIMUM_NUMBER_OF_DYNOS
           new_quantity = number_of_dynos - 1
