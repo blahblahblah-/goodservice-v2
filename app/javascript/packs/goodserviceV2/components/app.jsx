df5995af26e05496dbe79c440c93791fdb189aeb
Sunny Ng <sng@squarespace.com>
2021-01-20 00:11:42 -0500

    Another go with CSS

--- app/javascript/packs/goodserviceV2/components/app.jsx
+++ app/javascript/packs/goodserviceV2/components/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Header } from "semantic-ui-react";
-import 'semantic-ui-css/semantic.min.css'
+import './app.scss';
 
 class App extends React.Component {
   render() {
