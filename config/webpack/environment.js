8262756ac6c1bd16902e88a56d114d6afba65a39
Sunny Ng <sng@squarespace.com>
2021-01-20 02:01:16 -0500

    Another try

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader',
+  options: {
+    attempts: 1
+  }
+});
+
 module.exports = environment
