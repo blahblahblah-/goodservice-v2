e3fbc35a3eb942f028bc8ab44c01bf5a018171f5
Sunny Ng <sng@squarespace.com>
2021-01-22 21:49:45 -0500

    Allow debugging when running on dev

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,4 +1,4 @@
 Delayed::Worker.max_attempts = 1
-Delayed::Worker.max_run_time = 1.minute
+Delayed::Worker.max_run_time = Rails.env.production? ? 1.minute : 10.minutes
 Delayed::Worker.read_ahead = 2
 Delayed::Worker.sleep_delay = 1
