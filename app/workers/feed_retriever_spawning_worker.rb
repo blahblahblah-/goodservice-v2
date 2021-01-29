ef038deddcc5a366ba0682cb9057fb0be233b5b5
Sunny Ng <sng@squarespace.com>
2021-01-29 02:21:09 -0500

    Prioritize spawning worker

--- app/workers/feed_retriever_spawning_worker.rb
+++ app/workers/feed_retriever_spawning_worker.rb
@@ -1,5 +1,7 @@
 class FeedRetrieverSpawningWorker
   include Sidekiq::Worker
+  sidekiq_options retry: false, queue: 'critical'
+
   BASE_URI = "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs"
   FEEDS = ["", "-ace", "-bdfm", "-g", "-jz", "-nqrw", "-l", "-7", "-si"]
 
