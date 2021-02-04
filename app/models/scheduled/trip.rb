93d282d92e471636d811dd8f67a02426b9b05059
Sunny Ng <sng@squarespace.com>
2021-02-04 01:11:08 -0500

    Add explicit ordering for trip -> stop_times relationship

--- app/models/scheduled/trip.rb
+++ app/models/scheduled/trip.rb
@@ -1,7 +1,7 @@
 class Scheduled::Trip < ActiveRecord::Base
   belongs_to :route, foreign_key: "route_internal_id", primary_key: "internal_id"
   belongs_to :schedule, foreign_key: "schedule_service_id", primary_key: "service_id"
-  has_many :stop_times, foreign_key: "trip_internal_id", primary_key: "internal_id"
+  has_many :stop_times, -> { order("departure_time") }, foreign_key: "trip_internal_id", primary_key: "internal_id"
 
   DAY_IN_MINUTES = 86400
 
