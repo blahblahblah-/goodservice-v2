eba0f228197a85956806013ff1a6e4b0ecbb663b
Sunny Ng <sng@squarespace.com>
2021-04-06 10:08:49 -0400

    It's really hard to code without being to run it, I swear

--- app/controllers/api/slack_controller.rb
+++ app/controllers/api/slack_controller.rb
@@ -263,7 +263,7 @@ class Api::SlackController < ApplicationController
         "type": "section",
         "text": {
           "type": "mrkdwn",
-          "text": stop.secondary_name ? "*#{stop.name}* (#{stop.secondary_name}) - #{routes_stop_at.join(', ')}" : "*#{stop.name}* - #{routes_stop_at.join(', ')}"
+          "text": stop.secondary_name ? "*#{stop.stop_name}* (#{stop.secondary_name}) - #{routes_stop_at.join(', ')}" : "*#{stop.stop_name}* - #{routes_stop_at.join(', ')}"
         }
       }
     ]
