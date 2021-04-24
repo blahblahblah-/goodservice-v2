9b21bda5c5fd2e8f0b889a968ab4dea99a7d0e4b
Sunny Ng <sng@squarespace.com>
2021-04-24 16:50:06 -0400

    Consolidate TwitterDelaysNotifierWorker to use the goodservice Twitter app

--- app/workers/twitter_delays_notifier_worker.rb
+++ app/workers/twitter_delays_notifier_worker.rb
@@ -229,8 +229,8 @@ class TwitterDelaysNotifierWorker
     @twitter_client ||= Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV["TWITTER_TEST_CONSUMER_KEY"]
       config.consumer_secret     = ENV["TWITTER_TEST_CONSUMER_SECRET"]
-      config.access_token        = ENV["TWITTER_TEST_ACCESS_TOKEN"]
-      config.access_token_secret = ENV["TWITTER_TEST_ACCESS_TOKEN_SECRET"]
+      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
     end
   end
 
