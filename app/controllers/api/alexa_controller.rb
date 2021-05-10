e2a1f2805ac6a592e05aefcd2fe8575f44aafb01
Sunny Ng <sng@squarespace.com>
2021-05-10 11:46:42 -0400

    Fix typo that brought the site down

--- app/controllers/api/alexa_controller.rb
+++ app/controllers/api/alexa_controller.rb
@@ -109,7 +109,7 @@ class Api::AlexaController < ApplicationController
           strs = ["There are no upcoming train arrivals for #{stop.normalized_full_name}."]
         end
 
-        if elevator_advisories.[stop.internal_id].present?
+        if elevator_advisories[stop.internal_id].present?
           elevator_advisories[stop.internal_id].each { |a|
             strs << "The elevator for #{a} is out of service."
           }
