248510d8f14ab6ac6611c8b8640a285811622e15
Sunny Ng <sng@squarespace.com>
2021-05-20 19:01:31 -0400

    Announce service change summaries before service summaries for Alexa and Slack. Fix pronounciation of Dyre Av

--- app/controllers/api/alexa_controller.rb
+++ app/controllers/api/alexa_controller.rb
@@ -8,6 +8,7 @@ class Api::AlexaController < ApplicationController
     "schermerhorn" => "ʃɜrmərhərn",
     "kosciuszko" => "ˌkɒʒiˈʊʃkoʊ",
     "tremont" => "tɹimɑnt",
+    "dyre" => "ˈdaɪɚ",
   }
 
   def index
@@ -185,7 +186,7 @@ class Api::AlexaController < ApplicationController
         strs = ["The current status of the #{route_name} is #{status}."]
 
         if route_data.present?
-          summaries = route_data['service_summaries'].map { |_, summary| summary }.compact + route_data['service_change_summaries'].flat_map { |_, summary| summary}.compact
+          summaries = route_data['service_change_summaries'].flat_map { |_, summary| summary}.compact + route_data['service_summaries'].map { |_, summary| summary }.compact
           summaries.each do |summary|
             strs << summary.gsub(/\//, ' ').gsub(/\(\((.*?)\)\)/) do |stop_name|
               Scheduled::Stop.normalized_partial_name($1)
