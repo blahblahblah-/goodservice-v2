b9f19b67aa88a66837dbb7e3946754a87a677110
Sunny Ng <sng@squarespace.com>
2021-08-25 00:35:15 -0400

    Update config for ngrok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,5 +75,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << /[a-z0-9]+\.ngrok\.io/
+  config.hosts << /[a-z0-9\-]+\.ngrok\.io/
 end
