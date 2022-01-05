baac94899c3cd02613c8fe33458a62065f736cb7
Sunny Ng <sng@squarespace.com>
2022-01-05 10:43:48 -0500

    Fix bug with truncated service change logic

--- app/models/service_changes/truncated_service_change.rb
+++ app/models/service_changes/truncated_service_change.rb
@@ -1,7 +1,7 @@
 class ServiceChanges::TruncatedServiceChange < ServiceChanges::ServiceChange
   def applicable_to_routing?(routing)
     if begin_of_route?
-      routing.last == destination
+      destinations.include?(routing.last)
     else
       routing.last == first_station
     end
