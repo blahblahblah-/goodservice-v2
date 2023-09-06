16e6461d8df23bb72ad390509143c1a3dc2089ab
Sunny Ng <sunny@sunny-ng.com>
2023-09-06 02:51:31 -0400

    Improve labeling express/local routing logic for dropdown

--- app/javascript/packs/components/trainModalDirectionPane.jsx
+++ app/javascript/packs/components/trainModalDirectionPane.jsx
@@ -477,8 +477,24 @@ class TrainModalDirectionPane extends React.Component {
     const options = Object.keys(routings).map((hash) => {
       const routing = routings[hash];
       const allRoutings = Object.keys(routings).filter((key) => key !== hash).map((key) => routings[key]);
-      const isLocal = allRoutings.some((r) => r.every((s) => routing.includes(s)) && !routing.every((s) => r.includes(s)));
-      const isExpress = allRoutings.some((r) => !r.every((s) => routing.includes(s)) && routing.every((s) => r.includes(s)));
+      const isLocal = allRoutings.some((r) => {
+        let commonStop = routing.find((s) => r.includes(s));
+        if (!commonStop) {
+          return false;
+        }
+        let subrouting1 = routing.slice(routing.indexOf(commonStop));
+        let subrouting2 = r.slice(r.indexOf(commonStop));
+        return subrouting2.every((s) => subrouting1.includes(s)) && !subrouting1.every((s) => subrouting2.includes(s))
+      });
+      const isExpress = allRoutings.some((r) => {
+        let commonStop = routing.find((s) => r.includes(s));
+        if (!commonStop) {
+          return false;
+        }
+        let subrouting1 = routing.slice(routing.indexOf(commonStop));
+        let subrouting2 = r.slice(r.indexOf(commonStop));
+        return !subrouting2.every((s) => subrouting1.includes(s)) && subrouting1.every((s) => subrouting2.includes(s));
+      });
       let text = `${formatStation(stations[routing[0]].name)} ➜ ${formatStation(stations[routing[routing.length - 1]].name)} (${routing.length} stops)`;
       if (isLocal) {
         text = `${formatStation(stations[routing[0]].name)} ➜ ${formatStation(stations[routing[routing.length - 1]].name)} (${routing.length} stops via Local)`;
