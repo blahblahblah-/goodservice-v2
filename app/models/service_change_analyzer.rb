3202644a5e4d0c73bff69f80a9bc6cd69da7f023
Sunny Ng <sunny@sunny-ng.com>
2024-04-22 22:39:36 -0400

    Fix for checking against long term service changes

--- app/models/service_change_analyzer.rb
+++ app/models/service_change_analyzer.rb
@@ -293,8 +293,6 @@ class ServiceChangeAnalyzer
 
       [current_long_term_routings, current_route_routings, current_evergreen_routings, long_term_routings, evergreen_routings, current, recent_route_routings].each do |routing_set|
         route_pair = routing_set.find do |route_id, direction|
-          next false if long_term_routings[route_id].present? && route_id.first == current_route_id.first
-          next false if !reroute_service_change.begin_of_route? && !reroute_service_change.end_of_route? && route_id.first == current_route_id.first
           direction&.any? do |_, routings|
             station_combinations.any? do |sc|
               routings.any? do |r|
