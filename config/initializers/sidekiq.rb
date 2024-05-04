4e4a5f622e02af59b68958d2d4f09d8519a987ef
Sunny Ng <sunny@sunny-ng.com>
2024-05-04 19:25:06 -0400

    Wrap Sidekiq::Cron::Job.create statements

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,14 +8,16 @@ end
 
 Sidekiq.options[:poll_interval] = 2
 
-Sidekiq::Cron::Job.create(name: 'RoutingRefreshWorker - Every 30 secs', cron: '*/30 * * * * *', class: 'RoutingRefreshWorker')
-Sidekiq::Cron::Job.create(name: 'FeedRetrieverSpawningAWorker - Every 15 secs', cron: '0-46/15 * * * * *', class: 'FeedRetrieverSpawningAWorker')
-Sidekiq::Cron::Job.create(name: 'FeedRetrieverSpawningB1Worker - Every 15 secs', cron: '5-51/15 * * * * *', class: 'FeedRetrieverSpawningB1Worker')
-Sidekiq::Cron::Job.create(name: 'FeedRetrieverSpawningB2Worker - Every 15 secs', cron: '10-59/15 * * * * *', class: 'FeedRetrieverSpawningB2Worker')
-Sidekiq::Cron::Job.create(name: 'AccessibilityListWorker - Every 5 mins', cron: '*/5 * * * *', class: 'AccessibilityListWorker')
-Sidekiq::Cron::Job.create(name: 'AccessibilityStatusesWorker - Every 5 mins', cron: '*/5 * * * *', class: 'AccessibilityStatusesWorker')
-Sidekiq::Cron::Job.create(name: 'TwitterDelaysNotifierWorker - Every 1 min', cron: '* * * * *', class: 'TwitterDelaysNotifierWorker')
-Sidekiq::Cron::Job.create(name: 'TwitterServiceChangesNotifierWorker - Every 1 min', cron: '* * * * *', class: 'TwitterServiceChangesNotifierWorker')
-Sidekiq::Cron::Job.create(name: 'HerokuAutoscalerWorker - Every 1 min', cron: '* * * * *', class: 'HerokuAutoscalerWorker')
-Sidekiq::Cron::Job.create(name: 'TravelTimesRefreshWorker - Every 2 min', cron: '*/2 * * * *', class: 'TravelTimesRefreshWorker')
-Sidekiq::Cron::Job.create(name: 'RedisCleanupWorker - Every 30 mins', cron: '*/30 * * * *', class: 'RedisCleanupWorker')
+Rails.application.reloader.to_prepare do
+  Sidekiq::Cron::Job.create(name: 'RoutingRefreshWorker - Every 30 secs', cron: '*/30 * * * * *', class: 'RoutingRefreshWorker')
+  Sidekiq::Cron::Job.create(name: 'FeedRetrieverSpawningAWorker - Every 15 secs', cron: '0-46/15 * * * * *', class: 'FeedRetrieverSpawningAWorker')
+  Sidekiq::Cron::Job.create(name: 'FeedRetrieverSpawningB1Worker - Every 15 secs', cron: '5-51/15 * * * * *', class: 'FeedRetrieverSpawningB1Worker')
+  Sidekiq::Cron::Job.create(name: 'FeedRetrieverSpawningB2Worker - Every 15 secs', cron: '10-59/15 * * * * *', class: 'FeedRetrieverSpawningB2Worker')
+  Sidekiq::Cron::Job.create(name: 'AccessibilityListWorker - Every 5 mins', cron: '*/5 * * * *', class: 'AccessibilityListWorker')
+  Sidekiq::Cron::Job.create(name: 'AccessibilityStatusesWorker - Every 5 mins', cron: '*/5 * * * *', class: 'AccessibilityStatusesWorker')
+  Sidekiq::Cron::Job.create(name: 'TwitterDelaysNotifierWorker - Every 1 min', cron: '* * * * *', class: 'TwitterDelaysNotifierWorker')
+  Sidekiq::Cron::Job.create(name: 'TwitterServiceChangesNotifierWorker - Every 1 min', cron: '* * * * *', class: 'TwitterServiceChangesNotifierWorker')
+  Sidekiq::Cron::Job.create(name: 'HerokuAutoscalerWorker - Every 1 min', cron: '* * * * *', class: 'HerokuAutoscalerWorker')
+  Sidekiq::Cron::Job.create(name: 'TravelTimesRefreshWorker - Every 2 min', cron: '*/2 * * * *', class: 'TravelTimesRefreshWorker')
+  Sidekiq::Cron::Job.create(name: 'RedisCleanupWorker - Every 30 mins', cron: '*/30 * * * *', class: 'RedisCleanupWorker')
+end
